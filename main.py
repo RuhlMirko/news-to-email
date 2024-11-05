e6b7916f3f446cb9be3c6d82931404fee15c07ca
Mirko <ruhlmirkojoel@gmail.com>
2024-11-04 22:33:29 -0300

    Added system api key

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
+import os
 
-url = "https://finance.yahoo.com"
+api_key = os.getenv("NEWS_API_KEY")
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-10-05&sortBy=publishedAt&apiKey={api_key}"
 
 request = requests.get(url)
-content = request.text
+content = request.json()
 print(content)
+
+# for article in content['articles']:
+#     print(article['title'])
+#     print(article['description'])
+#     print()
 
