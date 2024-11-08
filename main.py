bca5a96d6b9bf440e26871a2f8a48ef738639cde
Mirko <ruhlmirkojoel@gmail.com>
2024-11-08 17:20:39 -0300

    Added env variable

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import os
 import send_email
 
-api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-10-05&sortBy=publishedAt&apiKey={api_key}"
+api_key = os.getenv("NEWS")
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-10-08&sortBy=publishedAt&apiKey={api_key}"
 
 request = requests.get(url)
 content = request.json()
@@ -11,7 +11,10 @@ content = request.json()
 
 message = "Subject: 'News of tesla'\n\n"
 for article in content['articles']:
-    message += article['title'] + "\n" + article['description'] + "\n\n"
-send_email.send_email_w(message)
+    # message += article['title'] + "\n" + article['description'] + "\n\n"
+
+    message += f"{article['title']}\n {article['description']}\n {article['url']}\n\n"
+# send_email.send_email_w(message)
+print(message)
 
 
